780f50b921744f8e457660a060eae63b4d32499d
JauchOnGitHub <jauch.github@gmail.com>
2013-11-05 16:43:11 +0000

    New Features
    + StringExtractor class
    + StringExtractor test case

---
+++ Toolbox/Tests/StringExtractorTest.cs
@@ -0,0 +1,84 @@
+﻿using Toolbox.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for StringExtractorTest and is intended
+    ///to contain all StringExtractorTest Unit Tests
+    ///</summary>
+   [TestClass()]
+   public class StringExtractorTest
+   {
+
+
+      private TestContext testContextInstance;
+
+      /// <summary>
+      ///Gets or sets the test context which provides
+      ///information about and functionality for the current test run.
+      ///</summary>
+      public TestContext TestContext
+      {
+         get
+         {
+            return testContextInstance;
+         }
+         set
+         {
+            testContextInstance = value;
+         }
+      }
+
+      #region Additional test attributes
+      // 
+      //You can use the following additional attributes as you write your tests:
+      //
+      //Use ClassInitialize to run code before running the first test in the class
+      //[ClassInitialize()]
+      //public static void MyClassInitialize(TestContext testContext)
+      //{
+      //}
+      //
+      //Use ClassCleanup to run code after all tests in a class have run
+      //[ClassCleanup()]
+      //public static void MyClassCleanup()
+      //{
+      //}
+      //
+      //Use TestInitialize to run code before running each test
+      //[TestInitialize()]
+      //public void MyTestInitialize()
+      //{
+      //}
+      //
+      //Use TestCleanup to run code after each test has run
+      //[TestCleanup()]
+      //public void MyTestCleanup()
+      //{
+      //}
+      //
+      #endregion
+
+
+      /// <summary>
+      ///A test for ExtractTimeIntervalFromString
+      ///</summary>
+      [TestMethod()]
+      public void ExtractTimeIntervalFromStringTest()
+      {
+         string input = "file_name_234_2012-10.01-2012x11-01.hdf5"; // TODO: Initialize to an appropriate value
+         string regex_pattern = null; // TODO: Initialize to an appropriate value
+         string date_pattern = null; // TODO: Initialize to an appropriate value
+         DatesInterval expected = new DatesInterval(new DateTime(2012, 10, 1, 0, 0, 0), new DateTime(2012, 11, 1, 0, 0, 0)); // TODO: Initialize to an appropriate value
+         DatesInterval actual;
+         actual = StringExtractor.ExtractTimeIntervalFromString(input, regex_pattern, date_pattern);
+         Assert.AreEqual(expected.Start, actual.Start);
+         Assert.AreEqual(expected.End, actual.End);
+         Assert.Inconclusive("Verify the correctness of this test method.");
+      }
+   }
+}
---
+++ Toolbox/Toolbox.Core/StringExtractor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Toolbox.Core
+{
+   public class DatesInterval
+   {
+      private DateTime f_start;
+      private DateTime f_end;
+
+      public DateTime Start 
+      { 
+         get 
+         {
+            return f_start;
+         }
+         set
+         {
+            f_start = value;
+         }
+      }
+      public DateTime End
+      {
+         get
+         {
+            return f_end;
+         }
+         set
+         {
+            f_end = value;
+         }
+      }
+
+      public DatesInterval(DateTime start, DateTime end)
+      {
+         f_start = start;
+         f_end = end;
+      }
+   }
+
+   /// <summary>
+   /// Extracts info from strings (like dates in file/folder names).
+   /// </summary>
+   public class StringExtractor
+   {
+      /// <summary>
+      /// Extract interval dates (start and end) from a string.
+      /// </summary>
+      /// <param name="input">String containing the dates.</param>
+      /// <param name="regex_pattern">Optional regex pattern for search the dates.</param>
+      /// <param name="date_pattern">Date pattern. Used only if regex_pattern is not null, in wich case it is mandatory.</param>
+      /// <returns>DateInterval with the values for the start and end interval dates.</returns>
+      public static DatesInterval ExtractTimeIntervalFromString(string input, string regex_pattern = null, string date_pattern = null)
+      {
+         MatchCollection matches_;
+
+         if (regex_pattern != null)
+         {
+            if (date_pattern == null)
+               throw new Exception("Missing date_pattern argument.");
+
+            matches_ = Regex.Matches(input, regex_pattern);
+            if (matches_.Count == 1) //start and end are in the format yyyy-MM-dd-HH
+            {
+               return new DatesInterval(DateTime.ParseExact(matches_[0].Groups["start"].Value, date_pattern, CultureInfo.InvariantCulture),
+                                        DateTime.ParseExact(matches_[0].Groups["end"].Value, date_pattern, CultureInfo.InvariantCulture));
+            }
+         }
+
+         matches_ = Regex.Matches(input, @"(?<start>\d{4}\D\d{2}\D\d{2}\D\d{2}).(?<end>\d{4}\D\d{2}\D\d{2}\D\d{2})");
+         if (matches_.Count == 1) //start and end are in the format yyyy-MM-dd-HH
+         {
+            return new DatesInterval(DateTime.ParseExact(Regex.Replace(matches_[0].Groups["start"].Value, @"\D", ""), "yyyyMMddHH", CultureInfo.InvariantCulture),
+                                     DateTime.ParseExact(Regex.Replace(matches_[0].Groups["end"].Value, @"\D", ""), "yyyyMMddHH", CultureInfo.InvariantCulture));
+         }
+
+         matches_ = Regex.Matches(input, @"(?<start>\d{4}\D\d{2}\D\d{2}).(?<end>\d{4}\D\d{2}\D\d{2})");
+         if (matches_.Count == 1) //start and end are in the format yyyy-MM-dd
+         {
+            return new DatesInterval(DateTime.ParseExact(Regex.Replace(matches_[0].Groups["start"].Value, @"\D", ""), "yyyyMMdd", CultureInfo.InvariantCulture),
+                                     DateTime.ParseExact(Regex.Replace(matches_[0].Groups["end"].Value, @"\D", ""), "yyyyMMdd", CultureInfo.InvariantCulture));
+         }
+
+         return null;
+      }
+
+   }
+}
